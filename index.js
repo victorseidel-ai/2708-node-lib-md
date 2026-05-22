9683eb251022c81077381ebe18f0d0e90d58c9ba
victorseidel-ai <victor.seidel@escola.pr.gov.br>
2026-05-22 12:40:53 +0000

    trabalho 2 finalizado

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+import fs from 'fs';
 import chalk from 'chalk';
+import { error } from 'console';
 
-console.log(chalk.red('Faça os tiros'), chalk.yellow('valerem a pena.'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/wait
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+  }
+  catch (erro) {
+    trataErro(erro)
+  }
+}
+
+// promises com then()
+
+//function pegaArquivo(caminhoDoArquivo) {
+   // const encoding = 'utf-8';
+    //.promises.readFile(caminhoDoArquivo, encoding)
+        //.then((texto) => console.log(chalk.green(texto)))
+        //catch(trataErro) 
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+  //  const encoding = 'utf-8';
+    //fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+      //  if (erro) {
+        //    trataErro(erro);
+        //}
+        //console.log(chalk.green(texto));
+    //})
+//
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
+//console.log(chalk.red('Faça os tiros'), chalk.yellow('valerem a pena.'));
